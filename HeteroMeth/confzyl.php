78f5390f046a6eabc4c3753a699219cd4e5fe03a
SophieHuan <45580316+SophieHuan@users.noreply.github.com>
2022-09-15 12:11:21 +0800

    Add files via upload

---
+++ HeteroMeth/confzyl.php
@@ -0,0 +1,7 @@
+<?php
+$DB_SERVER = "localhost";
+//$DB_SERVER = "210.73.40.61";
+$DB_NAME = "heterometh";
+$DB_USER = "zhangyl";
+$DB_PASS = "qianlab";
+?>
---
+++ HeteroMeth/rice.php
@@ -0,0 +1,2 @@
+<?php
+echo "<font color = red>ERROR: TCP non-blocking connect() to qianlab.genetics.ac.cn timed-out in select() after 10000 milliseconds - Cancelling! Couldn't open http://qianlab.genetics.ac.cn/HeteroMeth/Files/UCSC/Rice/genomes.txt</font>" ?>
